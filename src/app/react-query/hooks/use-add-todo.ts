30f64ee67403532f748b642ff8915a81a053ea09
Akshay Vadher <2619644+akshayvadher@users.noreply.github.com>
2024-03-10 16:31:39 +0530

    Optimistic updates on add

--- src/app/react-query/hooks/use-add-todo.ts
+++ src/app/react-query/hooks/use-add-todo.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { delay, TODO_URL } from "@/app/react-query/hooks/common";
 import { Status } from "@/app/react-query/todo.dto";
@@ -17,9 +17,34 @@ export interface ToDoAddRequestDto {
 }
 
 export const useAddTodo = (options?: { onSuccess?: () => void }) => {
+  const queryClient = useQueryClient();
   const add = useMutation({
     mutationFn: addToDoFn,
     onSuccess: options?.onSuccess,
+    // When mutate is called:
+    onMutate: async (newTodo) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: ["todos"] });
+
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData(["todos"]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(["todos"], (old: any) => [...old, newTodo]);
+
+      // Return a context object with the snapshotted value
+      return { previousTodos };
+    },
+    // If the mutation fails,
+    // use the context returned from onMutate to roll back
+    onError: (err, newTodo, context) => {
+      queryClient.setQueryData(["todos"], context?.previousTodos);
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
   });
   return add;
 };
